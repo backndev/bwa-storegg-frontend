cf3761040bf0e49b8783ea7df044dd7bd430bb66
backndevv <ramseshutasoit77@gmail.com>
2021-11-26 23:05:30 +0700

    #4 create voucher data types

--- services/data-types/index.ts
+++ services/data-types/index.ts
@@ -1,13 +1,35 @@
-exports interface CategoryTypes {
+export interface CategoryTypes {
     _id: string;
     name: string;
     __v: number;
 }
 
-exports interface GameItemTypes {
+export interface GameItemTypes {
     _id: string;
     status: string;
     name: string;
     thumbnail: string;
     category: CategoryTypes;
 }
+
+export interface BankTypes {
+    _id: string;
+    name: string;
+    nameBank: string;
+    noRekening: string;
+}
+
+export interface PaymentTypes {
+    _id: string;
+    type: string;
+    status: string;
+    banks: BankTypes[]
+}
+
+export interface NominalsTypes {
+    _id: string;
+    coinQuantity: number;
+    coinName: string;
+    price: number;
+}
+
